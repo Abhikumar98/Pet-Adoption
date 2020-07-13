dbb7a236f5bb902e6ac5a74417f98621b4213f16
Abhikumar98 <abhishekkumar35962@gmail.com>
2020-07-13 21:37:28 +0530

    Updates

---
+++ precache-manifest.fab727a53ed56d863e2e39d993b4129c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df47d74791fc59f09db729376e62cdc2",
+    "url": "/Pet-Adoption/index.html"
+  },
+  {
+    "revision": "9dede97e4d76bc333d9a",
+    "url": "/Pet-Adoption/static/css/main.7ba63ecf.chunk.css"
+  },
+  {
+    "revision": "524b6f60bca4cea93463",
+    "url": "/Pet-Adoption/static/js/2.1916a3fb.chunk.js"
+  },
+  {
+    "revision": "9dede97e4d76bc333d9a",
+    "url": "/Pet-Adoption/static/js/main.06350f89.chunk.js"
+  },
+  {
+    "revision": "99f2355c7aaefe581b84",
+    "url": "/Pet-Adoption/static/js/runtime~main.72867f80.js"
+  }
+]);
